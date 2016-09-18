943cab24fefbaed24cdc8ceab42834abf59c550a
Hillel Kahana <hillel@kontera.com>
2016-09-19 01:39:00 +0300

    adding_iter_permutation

--- permutation.rb
+++ permutation.rb
@@ -12,12 +12,36 @@ def permutation arr
   }
 end
 
+def permutation_iter arr
+	final_arr = [arr.last(2), arr.last(2).reverse].uniq
+	current_inject_count = 2
+	(arr.size - 3).downto(0).each {|i|
+	  temp_arr = []
+	  current_inject_count.downto(0).each{|place|
+			 temp_arr += final_arr.map{|in_arr| in_arr.dup.insert(place, arr[i]) }
+	  }
+		current_inject_count += 1
+		final_arr = temp_arr
+	}
+	final_arr
+end
 
 def riddle input
   permutation(input).max_by {|a| a[0] * (10*a[1] + a[2]) * (100*a[3] + 10*a[4] + a[5]) }
-  # ruby (c) native: input.permutation.max_by {|a| a[0] * (10*a[1] + a[2]) * (100*a[3] + 10*a[4] + a[5]) }
+end
+
+def riddle_native input
+  input.permutation.max_by {|a| a[0] * (10*a[1] + a[2]) * (100*a[3] + 10*a[4] + a[5]) }
 end
 
 input = [1,2,3,4]
-#permutation input
-riddle input
+p " ====== permutation ====== "
+p permutation_iter input
+puts "   user      system      real      total"
+puts Benchmark.measure { permutation input }
+puts Benchmark.measure { permutation_iter input }
+puts Benchmark.measure { input.permutation }
+puts " ====== riddle ====== "
+input = [1,2,3,4,5,6]
+puts Benchmark.measure { riddle_native input }
+
